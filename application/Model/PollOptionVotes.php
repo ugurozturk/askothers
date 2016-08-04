6b8619470d964976b996f2262e1cbdfc0a13a0ea
ugurozturk <tarantulaugur@hotmail.com>
2016-08-04 10:52:33 +0300

    Modeller Oluşturuldu

--- application/Model/PollOptionVotes.php
+++ application/Model/PollOptionVotes.php
@@ -18,23 +18,56 @@ class PollOptionVotes extends Model
 {
     public function getAllPollOptionVotes()
     {
+        $sql = "SELECT poll_option_vote_id, poll_option_id, user_id, created_date FROM poll_option_votes";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+
+        return $query->fetchAll();
     }
 
-    public function addPollOptionVotes()
+    public function addPollOptionVotes($poll_option_id, $user_id)
     {
+        $sql = "INSERT INTO poll_option_votes (poll_option_id, user_id, created_date) VALUES (:poll_option_id, :user_id)";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':poll_option_id' => $poll_option_id, 'user_id' => $user_id);
+
+        $query->execute($parameters);
     }
 
-    public function deletePollOptionVotes()
+    public function deletePollOptionVotes($poll_option_vote_id)
     {
+        $sql = "DELETE FROM poll_option_votes WHERE poll_option_vote_id = :poll_option_vote_id";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':poll_option_vote_id' => $poll_option_vote_id);
+
+        $query->execute($parameters);
     }
-    public function getPollOptionVotes()
+
+    public function getPollOptionVotes($poll_option_vote_id)
     {
+        $sql = "SELECT poll_option_vote_id, poll_option_id, user_id, created_date FROM poll_option_votes WHERE poll_option_vote_id = :poll_option_vote_id LIMIT 1";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':poll_option_vote_id' => $poll_option_vote_id);
+        $query->execute($parameters);
+
+        return $query->fetch();
     }
-    public function updatePollOptionVotes()
+
+    public function updatePollOptionVotes($poll_option_vote_id, $poll_option_id, $user_id)
     {
+        $sql = "UPDATE poll_option_votes SET poll_option_id = :poll_option_id, user_id = :user_id WHERE poll_option_vote_id = :poll_option_vote_id";
+        $query = $this->db->prepare($sql);
+        $parameters = array(':poll_option_vote_id' => $poll_option_vote_id, ':poll_option_id' => $poll_option_id, ':user_id' => $user_id);
+
+        $query->execute($parameters);
     }
 
     public function getAmountOfPollOptionVotes()
     {
+        $sql = "SELECT COUNT(poll_option_vote_id) AS amount_of_polloptionvotes FROM poll_option_votes";
+        $query = $this->db->prepare($sql);
+        $query->execute();
+
+        return $query->fetch()->amount_of_polloptionvotes;
     }
 }
