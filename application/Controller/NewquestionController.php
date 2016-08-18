e3f851e0d2bd0541893be7fa0d66dddebadb9f7b
ugurozturk <tarantulaugur@hotmail.com>
2016-08-19 00:57:29 +0300

    Clean Code

--- application/Controller/NewquestionController.php
+++ application/Controller/NewquestionController.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * Class HomeController
- *
- * Please note:
- * Don't use the same name for class and method, as this might trigger an (unintended) __construct of the class.
- * This is really weird behaviour, but documented here: http://php.net/manual/en/language.oop5.decon.php
- *
- */
-
 namespace Mini\Controller;
 
 use Mini\Model\Questions;
@@ -17,10 +8,6 @@ use Mini\Model\PollOptionVotes;
 
 class NewquestionController
 {
-    /**
-     * PAGE: index
-     * This method handles what happens when you move to http://yourproject/home/index (which is the default page btw)
-     */
     public function index()
     {
       if(isset($_SESSION["user_id"]) && !empty($_SESSION["user_id"])){
--- application/Controller/ShuffleController.php
+++ application/Controller/ShuffleController.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * Class HomeController
- *
- * Please note:
- * Don't use the same name for class and method, as this might trigger an (unintended) __construct of the class.
- * This is really weird behaviour, but documented here: http://php.net/manual/en/language.oop5.decon.php
- *
- */
-
 namespace Mini\Controller;
 
 use Mini\Model\Questions;
@@ -19,10 +10,6 @@ use Mini\Model\Points;
 
 class ShuffleController
 {
-    /**
-     * PAGE: index
-     * This method handles what happens when you move to http://yourproject/home/index (which is the default page btw)
-     */
     public function index()
     {
         if(isset($_SESSION["user_id"]) && !empty($_SESSION["user_id"])){
